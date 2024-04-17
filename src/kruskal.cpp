49a8fff4543629343bf9c8ddc58f440691a22fbe
Jackson Casey <65034067+jaxcksn@users.noreply.github.com>
2024-04-16 20:45:35 -0500

    Added Kruskal's Algorithm

--- src/kruskal.cpp
+++ src/kruskal.cpp
@@ -1,7 +1,35 @@
 #include "Graph.hpp"
+#include "kruskal.hpp"
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 MST kruskal_mst(Graph &graph)
 {
     MST mst;
+    EdgeList edges(graph);
+    UnionFind unionFind(graph.vertNumber());
+
+    // We'll sort the edge list by weight, using the sort function
+    sort(edges.list.begin(), edges.list.end(), [](const Graph::Edge &a, const Graph::Edge &b)
+         { return a.weight < b.weight; });
+
+    for (const auto &edge : edges.list)
+    {
+        if (unionFind.find(edge.src) != unionFind.find(edge.dest))
+        {
+            unionFind.merge(edge.src, edge.dest);
+            mst.edges.push_back(edge);
+            mst.totalWeight += edge.weight;
+
+            if (mst.edges.size() == graph.vertNumber() - 1)
+            {
+                // We've reached full size for the MST, we can exit now.
+                break;
+            }
+        }
+    }
+
     return mst;
 }
--- src/kruskal.hpp
+++ src/kruskal.hpp
@@ -4,36 +4,76 @@
 #include "Graph.hpp"
 #include "algorithm"
 #include "unordered_map"
+#include <numeric>
 
+using namespace std;
 MST kruskal_mst(Graph &graph);
 
+class EdgeList
+{
+
+public:
+    vector<Graph::Edge> list;
+    explicit EdgeList(Graph g)
+    {
+        for (int i = 0; i < g.vertNumber(); i++)
+        {
+            for (const auto &[dest, weight] : g.adjList[i])
+            {
+                if (i < dest)
+                {
+                    list.emplace_back(i, dest, weight);
+                }
+            }
+        }
+    }
+};
+
+// Implementation of DSU or Union Find data structure, used by Kruskal's algorithm.
 class UnionFind
 {
+private:
+    // The parent vector
+    vector<int> parent;
+    // The rank vector
+    vector<int> rank;
+
 public:
-    std::unordered_map<int, int> parent;
-
-    int find(int u)
+    // Constructor
+    explicit UnionFind(int numElements) : parent(numElements), rank(numElements, 0)
     {
-        if (parent.find(u) == parent.end())
-        {
-            return u;
-        }
-
-        if (parent[u] != u)
-        {
-            parent[u] = find(parent[u]);
-        }
-
-        return parent[u];
+        // Fill the parent with integers.
+        iota(parent.begin(), parent.end(), 0);
     }
 
-    void merge(int u, int v)
+    // Recusrive implementation of Find algorithm for the Union Find
+    int find(int x)
     {
-        int rootU = find(u);
-        int rootV = find(v);
-        if (rootU != rootV)
+        if (parent[x] != x)
         {
-            parent[rootU] = rootV;
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    // Rank based set union, called "merge" since union is a reserved word in C++
+    void merge(int x, int y)
+    {
+        int xRoot = find(x);
+        int yRoot = find(y);
+
+        if (rank[xRoot] > rank[yRoot])
+        {
+            parent[yRoot] = xRoot;
+        }
+        else if (rank[xRoot] < rank[yRoot])
+        {
+            parent[xRoot] = yRoot;
+        }
+        else
+        {
+            parent[yRoot] = xRoot;
+            rank[xRoot] += 1;
         }
     }
 };
